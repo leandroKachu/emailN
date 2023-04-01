ab5d8f667431fece7311c0171e8618a9277909ba
leandroKachu <lesilvajunior@hotmail.com>
2023-04-01 15:35:47 -0700

    add a unique id, adjust my test file

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,6 +2,7 @@ package campaign
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -10,16 +11,38 @@ import (
 // A1 = organization
 // A2 = action
 // A3 = assert
-func TestNewCampaign(t *testing.T) {
+
+var (
+	name     = "NewCampaign"
+	content  = "body"
+	contacts = []string{"email1@example.com", "email2@example.com"}
+)
+
+func Test_NewCampaign(t *testing.T) {
 	//A1
 	assert := assert.New(t)
-	name := "NewCampaign"
-	content := "body"
-	contacts := []string{"email1@example.com", "email2@example.com"}
 	//A2
 	campaign := NewCampaign(name, content, contacts)
 	//A3 assert
-	assert.Equal(campaign.ID, "1")
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
+}
+
+func Test_NewCampaign_IDIsNotNil(t *testing.T) {
+	//A1
+	assert := assert.New(t)
+	//A2
+	campaign := NewCampaign(name, content, contacts)
+	//A3 assert
+	assert.NotNil(campaign.ID)
+}
+
+func Test_NewCampaign_CreateOnMustBeNow(t *testing.T) {
+	//A1
+	assert := assert.New(t)
+	now := time.Now().Add(-time.Minute)
+	//A2
+	campaign := NewCampaign(name, content, contacts)
+	//A3 assert
+	assert.Greater(campaign.CreatedOn, now)
 }
