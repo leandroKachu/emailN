1df44eca6e8ed9c0be47ee132aa4081216995ea6
leandroKachu <lesilvajunior@hotmail.com>
2023-04-01 15:13:28 -0700

    add a package for global unique id generator

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -6,15 +6,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// organizacao 3 A
+// A1 = organization
+// A2 = action
+// A3 = assert
 func TestNewCampaign(t *testing.T) {
+	//A1
 	assert := assert.New(t)
-
 	name := "NewCampaign"
 	content := "body"
 	contacts := []string{"email1@example.com", "email2@example.com"}
-
+	//A2
 	campaign := NewCampaign(name, content, contacts)
-
+	//A3 assert
 	assert.Equal(campaign.ID, "1")
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
