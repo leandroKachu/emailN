51a8f71fe3a723a21049cd0ccf4bfb097c0e41f5
leandroKachu <lesilvajunior@hotmail.com>
2023-04-03 20:07:15 -0700

    adjust unity test and bugs

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/jaswdr/faker"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -14,20 +15,20 @@ import (
 
 var (
 	name     = "NewCampaign"
-	content  = "body"
+	content  = "body !Hi"
 	contacts = []string{"email1@example.com", "email2@example.com"}
+	fake     = faker.New()
 )
 
-func Test_NewCampaign(t *testing.T) {
-	//A1
+func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	assert := assert.New(t)
-	//A2
+
 	campaign, _ := NewCampaign(name, content, contacts)
-	//A3 assert
+
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
+	assert.Equal(len(campaign.Contacts), len(contacts))
 }
-
 func Test_NewCampaign_IDIsNotNil(t *testing.T) {
 	//A1
 	assert := assert.New(t)
@@ -49,23 +50,44 @@ func Test_NewCampaign_CreateOnMustBeNow(t *testing.T) {
 
 // Testing errors handling ()()()()()()()()()()()()()()()()()()()()()()()()()
 
-func Test_NewCampaign_MustValidateName(t *testing.T) {
+func Test_NewCampaign_MustValidateNameMin(t *testing.T) {
 	assert := assert.New(t)
 
 	_, err := NewCampaign("", content, contacts)
-	assert.Equal("name must not be empty", err.Error())
+	assert.Equal("name is min5", err.Error())
 }
 
-func Test_NewCampaign_MustValidateContent(t *testing.T) {
+func Test_NewCampaign_MustValidateNameMax(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(fake.Lorem().Text(30), content, contacts)
+	assert.Equal("name is Max20", err.Error())
+}
+
+func Test_NewCampaign_MustValidateContentMin(t *testing.T) {
 	assert := assert.New(t)
 
 	_, err := NewCampaign(name, "", contacts)
-	assert.Equal("content must not be empty", err.Error())
+	assert.Equal("content is min5", err.Error())
 }
 
-func Test_NewCampaign_MustValidateContacts(t *testing.T) {
+func Test_NewCampaign_MustValidateContentMax(t *testing.T) {
 	assert := assert.New(t)
 
-	_, err := NewCampaign(name, content, []string{})
-	assert.Equal("contacts must not be empty", err.Error())
+	_, err := NewCampaign(name, fake.Lorem().Text(1040), contacts)
+	assert.Equal("content is Max1028", err.Error())
+}
+
+func Test_NewCampaign_MustValidateContactsMin(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(name, content, nil)
+	assert.Equal("contacts is min1", err.Error())
+}
+
+func Test_NewCampaign_MustValidateContactsMax(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(name, content, []string{"email_invalid"})
+	assert.Equal("email is invalid", err.Error())
 }
