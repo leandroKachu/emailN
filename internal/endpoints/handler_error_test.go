a9720495f1641da27b762f8b17f3c917d9406392
leandroKachu <lesilvajunior@hotmail.com>
2023-04-06 13:51:19 -0700

    add a handler error when return a obj

--- internal/endpoints/handler_error_test.go
+++ internal/endpoints/handler_error_test.go
@@ -2,7 +2,9 @@ package endpoints
 
 import (
 	internalerrors "emailn/internal/internal-errors"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -47,5 +49,34 @@ func Test_HanderError_when_endpoints_returns_domain_error(t *testing.T) {
 
 	assert.Equal(http.StatusBadRequest, res.Code)
 	assert.Contains(res.Body.String(), "bad request error")
+
+}
+
+func Test_HanderError_when_endpoints_returns_object(t *testing.T) {
+	assert := assert.New(t)
+
+	type ObjectNaruto struct {
+		ID   int
+		Name string
+	}
+
+	myobject := ObjectNaruto{ID: 1, Name: "Nagato"}
+
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return myobject, 201, nil
+	}
+	handlerFunc := HandlerError(endpoint)
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+	handlerFunc.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusCreated, res.Code)
+
+	personNarutoAnime := ObjectNaruto{}
+	fmt.Println(&personNarutoAnime)
+
+	json.Unmarshal(res.Body.Bytes(), &personNarutoAnime)
+	assert.Equal(myobject, personNarutoAnime)
 
 }
