9e487a33dc18296ac0deb24d2db8452354998b56
leandroKachu <lesilvajunior@hotmail.com>
2023-04-06 13:06:55 -0700

    add unit test for handlererror method when return 400

--- internal/endpoints/handler_error_test.go
+++ internal/endpoints/handler_error_test.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	internalerrors "emailn/internal/internal-errors"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -26,5 +27,25 @@ func Test_HanderError_when_endpoints_returns_internal_error(t *testing.T) {
 
 	assert.Equal(http.StatusInternalServerError, res.Code)
 	assert.Contains(res.Body.String(), internalerrors.ErrInternal.Error())
+
+}
+
+func Test_HanderError_when_endpoints_returns_domain_error(t *testing.T) {
+	assert := assert.New(t)
+
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return nil, 0, errors.New("bad request error")
+	}
+	handlerFunc := HandlerError(endpoint)
+
+	println(endpoint)
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusBadRequest, res.Code)
+	assert.Contains(res.Body.String(), "bad request error")
 
 }
